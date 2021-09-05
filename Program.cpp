3e8340fb784fa9216756bbae556650707c35724a
Sirajuddin Asjad <sirasjad@gmail.com>
2021-09-05 04:57:46 +0200

    Switched to smart pointers

--- Program.cpp
+++ Program.cpp
@@ -1,10 +1,7 @@
-#include <iostream>
-#include <string>
 #include <QtCore>
 #include <QCoreApplication>
 #include <QObject>
 
-//#include "include/MqttConnection.hpp"
 #include "include/MqttHandler.hpp"
 
 int main(int argc, char** argv)
@@ -15,7 +12,10 @@ int main(int argc, char** argv)
     QString broker_addr = "127.0.0.1";
     quint16 broker_port = 1883;
 
-    Mqtt::Handler* mqtt_handler = new Mqtt::Handler(broker_addr, broker_port);
+    /* Initialize a unique pointer to MQTT handler*/
+    QScopedPointer<Mqtt::Handler> mqtt_handler = QScopedPointer<Mqtt::Handler>(new Mqtt::Handler(broker_addr, broker_port));
+
+    /* Establish connection with MQTT broker */
     mqtt_handler->connect();
 
     a.exec();
